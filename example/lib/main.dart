e1b67054b1829eb0d883a6bf3a35c221eea813d7
Mayank Kushal <mayankkushal26@gmail.com>
2021-06-21 01:06:21 +0530

    upgrade to null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -47,7 +47,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   Map<String, dynamic> jsonObj;
   BuildContext tContext;
 
@@ -155,23 +154,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Builder(
-        builder: (context) {
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Builder(builder: (context) {
           tContext = context;
           return SafeArea(
             child: SingleChildScrollView(
                 // Center is a layout widget. It takes a single child and positions it
                 // in the middle of the parent.
-                child: JsonViewerWidget(jsonObj)
-            ),
+                child: JsonViewer(jsonObj)),
           );
-        }
-      )
-    );
+        }));
   }
 }
